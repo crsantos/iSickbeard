12d7aa735c34f072dc8010ebf45fae978b62e072
Carlos Santos <carlos.santos@blip.pt>
2015-01-30 18:35:39 +0000

    Initial Commit

---
+++ iSickbeard/Classes/Controllers/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iSickbeard
+//
+//  Created by Carlos Santos on 30/01/15.
+//  Copyright (c) 2015 Carlos Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
