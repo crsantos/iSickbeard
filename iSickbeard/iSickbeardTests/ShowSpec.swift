9e0b5eed0a5b41bae7f4b3ec77bafd0814f92b14
Carlos Santos <carlosricardosantos@gmail.com>
2015-02-01 22:34:54 +0000

    Updated Show model and tests

--- iSickbeard/iSickbeardTests/ShowSpec.swift
+++ iSickbeard/iSickbeardTests/ShowSpec.swift
@@ -10,14 +10,20 @@ class ShowSpec: QuickSpec {
             describe("as a representation of a tv show") {
                 
                 it("should be able to set its properties") {
-                    
+
                     let show = Show(
                         name: "name",
+                        indexerId: 12314,
+                        airs: "Sunday, 8/9Central",
+                        location: "/",
                         quality: QualitySetting.QualitySetting1080pBluRay
                     )
                     
                     expect(show.name).to(equal("name"))
                     expect(show.quality).to(equal(QualitySetting.QualitySetting1080pBluRay))
+                    expect(show.indexerId).to(equal(12314))
+                    expect(show.location).to(equal("/"))
+                    expect(show.airs).to(equal("Sunday, 8/9Central"))
                 }
             }
         }
