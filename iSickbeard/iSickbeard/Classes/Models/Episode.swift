88a53568ed29bee8013e2edf7ea33964a2968c83
Carlos Santos <carlosricardosantos@gmail.com>
2015-02-01 22:25:05 +0000

    Added Episode Model and EpisodeStatus enum

---
+++ iSickbeard/iSickbeard/Classes/Models/Episode.swift
@@ -0,0 +1,34 @@
+//
+//  Episode.swift
+//  iSickbeard
+//
+//  Created by Carlos Santos on 01/02/15.
+//  Copyright (c) 2015 Carlos Santos. All rights reserved.
+//
+
+import Foundation
+
+class Episode {
+    
+    var name:String
+    var airdate:NSDate
+    var file_size:Int           = 0
+    var location:String         = ""
+    var quality:QualitySetting  = QualitySetting.QualitySettingNA
+    var status:EpisodeStatus    = EpisodeStatus.EpisodeStatusNA
+    var release_name:String?
+    var subtitles:Array<String> = []
+    var seasonNumber:Int
+    
+    init(name:String, seasonNumber:Int, airDate:NSDate) {
+
+        self.name         = name
+        self.airdate      = airDate
+        self.seasonNumber = seasonNumber
+    }
+    
+    func updateSubtitles(subtitlesString:String) {
+        
+        self.subtitles = subtitlesString.componentsSeparatedByString(",")
+    }
+}
---
+++ iSickbeard/iSickbeard/Classes/Models/EpisodeStatus.swift
@@ -0,0 +1,17 @@
+//
+//  EpisodeStatus.swift
+//  iSickbeard
+//
+//  Created by Carlos Santos on 01/02/15.
+//  Copyright (c) 2015 Carlos Santos. All rights reserved.
+//
+
+enum EpisodeStatus:String {
+
+    case EpisodeStatusNA         = "N/A"
+    case EpisodeStatusWanted     = "Wanted"
+    case EpisodeStatusDownloaded = "Downloaded"
+    case EpisodeStatusSkipped    = "Skipped"
+    case EpisodeStatusUnaired    = "Unaired"
+}
+
