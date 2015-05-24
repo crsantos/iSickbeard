b3ecd58bc9cac2cdd5e2b19be4551f0d84747fe1
Carlos Santos <carlosricardosantos@gmail.com>
2015-05-24 01:03:15 +0100

    Added GlobalConstants file

---
+++ iSickbeard/iSickbeard/Classes/Util/GlobalConstants.swift
@@ -0,0 +1,14 @@
+//
+//  GlobalConstants.swift
+//  iSickbeard
+//
+//  Created by Carlos Santos on 24/05/15.
+//  Copyright (c) 2015 Carlos Santos. All rights reserved.
+//
+
+import Foundation
+
+struct JSONConstants {
+    
+    static let jsonDataKey = "data"
+}
