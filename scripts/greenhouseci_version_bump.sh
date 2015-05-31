c2c05fa0362c2ba5d7e81c757b5c61c385d65911
Carlos Santos <carlosricardosantos@gmail.com>
2015-05-31 12:14:12 +0100

    Fixed bumping the current version and not one extra increment Removed pushd to avoid getting out the xcodeproj dir

--- scripts/greenhouseci_version_bump.sh
+++ scripts/greenhouseci_version_bump.sh
@@ -10,11 +10,10 @@ if [ -z "$GREENHOUSE_BUILD" ]; then
     echo "var is unset";
     echo "Exiting"
 else
-    echo "!! var is set !!";
-    # BUILD_NUMBER=10
-    newBuildNumber=$((BUILD_NUMBER+1))
-    echo "bymping to $newBuildNumber"
-    pushd ./iSickbeard # go to the project dir
-    agvtool new-version -all $newBuildNumber
-    popd # get back to old dir
+    echo "!! GREENHOUSE_BUILD is active !!";
+    echo "Bumping to $BUILD_NUMBER"
+    ls -la
+    # pushd ./iSickbeard # go to the project dir
+    agvtool new-version -all $BUILD_NUMBER
+    # popd # get back to old dir
 fi
